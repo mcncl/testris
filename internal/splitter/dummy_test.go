8c918a78e374aabe95119b2f01ff3f8ef177713c
Ben McNicholl <git@benmcnicholl.com>
2024-11-22 16:20:25 +1100

    Add some dummy tests

---
+++ internal/splitter/dummy_test.go
@@ -0,0 +1,84 @@
+// internal/splitter/dummy_test.go
+package splitter
+
+import "testing"
+
+func TestAlphaFeature(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestBetaValidation(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestCharlieParsing(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestDeltaProcessing(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestEchoHandling(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestFoxtrotLogic(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestGolfAuthentication(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestHotelIntegration(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestIndiaPerformance(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestJulietCaching(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestKiloEncryption(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestLimaDecryption(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestMikeCompression(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestNovemberDecompression(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestOscarSerialization(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestPapaSynchronization(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestQuebecOptimization(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestRomeoNormalization(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestSierraValidation(t *testing.T) {
+	t.Skip("Dummy test")
+}
+
+func TestTangoIntegration(t *testing.T) {
+	t.Skip("Dummy test")
+}
